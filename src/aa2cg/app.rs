18dcb909b261d903637cccc36c728e3c9fe81ed0
Diego Ugarte <diego.ugarte@outlook.com>
2021-12-23 12:55:33 +0900

    Add color crate

--- src/aa2cg/app.rs
+++ src/aa2cg/app.rs
@@ -1,16 +1,18 @@
 use clap::ArgMatches;
 use crate::util::file;
+use ansi_term::Color;
 
 pub fn app(args: &ArgMatches) {
     let verbose = args.is_present("verbose");
 
     let input_filename = args.value_of("input").unwrap();
     if verbose {
-        println!("Input file: {input_filename}")
+        println!("{} {}", Color::Blue.paint("Input file: "),
+                          Color::Green.paint(input_filename));
     } 
 
     let extension = file::get_extension_from_filename(input_filename);
     if verbose {
-        println!("Input file format: {extension}")
+        println!("Input file format: {extension}");
     } 
 }
