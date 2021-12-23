8bd44c372055ff9ea13e34e1ae3defccf31fd958
Diego Ugarte <diego@pop-os.localdomain>
2021-12-23 20:09:40 +0900

    Add pdb parser

--- src/aa2cg/app.rs
+++ src/aa2cg/app.rs
@@ -1,18 +1,56 @@
+use std::fs;
+use std::path;
+
 use clap::ArgMatches;
-use crate::util::file;
-use ansi_term::Color;
+use colored::Colorize;
+
+use crate::molecule;
+use crate::util::mdfile;
 
 pub fn app(args: &ArgMatches) {
     let verbose = args.is_present("verbose");
 
-    let input_filename = args.value_of("input").unwrap();
     if verbose {
-        println!("{} {}", Color::Blue.paint("Input file: "),
-                          Color::Green.paint(input_filename));
+        println!("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+        println!("┃{:^58}┃", "AA2CG".bold());
+        println!("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+        println!("{}", "Input file information".bold());
+    }
+
+    let input_path = args.value_of("input").unwrap();
+    let input_path = path::Path::new(input_path);
+    let input_filename = input_path.file_name()
+                                   .unwrap()
+                                   .to_str()
+                                   .unwrap();
+    if verbose {
+        println!("├─ {} {}", "Input file:".blue().bold(), input_filename.green());
     } 
 
-    let extension = file::get_extension_from_filename(input_filename);
+    let extension = mdfile::get_extension_from_filename(input_filename);
     if verbose {
-        println!("Input file format: {extension}");
+        println!("├─ {} {}", "Input format:".blue().bold(), extension.to_string().green());
     } 
+
+    let mut file = match fs::File::open(input_path) {
+        Ok(f) => f,
+        Err(e) => {
+            if verbose {
+                println!("└─ {} {} {}", "Error:".red().bold(), "Can not open input file.", e);
+            } else {
+                println!("{} {} {}", "Error:".red().bold(), "Can not open input file.", e);
+            }
+            return;
+        },
+    };
+
+    let structure = match extension {
+        mdfile::FileExtension::Pdb => molecule::Structure::from_pdb_file(file, verbose),
+        _ => molecule::Structure::from_pdb_file(file, verbose),
+    };
+    if verbose {
+        println!("└──┬─ {} {}", "Atoms:".yellow().bold(), structure.atom_name.len());
+        println!("   ├─ {} {}", "Residues:".yellow().bold(), structure.residue_name.len());
+        println!("   └─ {} {}", "Chains:".yellow().bold(), structure.chain_name.len());
+    }
 }
--- src/molecule.rs
+++ src/molecule.rs
@@ -1,37 +1,123 @@
-#[allow(dead_code)]
-pub struct AAResidue {
-    name: String,
-    atoms: Vec<u64>,
+use std::fs;
+use std::process;
+use std::io::{self, BufRead};
+
+use colored::Colorize;
+
+#[derive(Debug, Default)]
+pub struct Structure {
+    pub atom_name: Vec<String>,
+    pub atom_index: Vec<u32>,
+    pub residue_name: Vec<String>,
+    pub residue_index: Vec<u32>,
+    pub chain_name: Vec<String>,
+    pub chain_index: Vec<u32>,
+    pub position: Vec<[f32; 3]>,
 }
 
-#[allow(dead_code)]
-pub struct AAChain {
-    id: String,
-    segname: String,
-    moltype: u32,
-    residues: Vec<AAResidue>,
-}
+impl Structure {
+    pub fn from_pdb_file(pdb: fs::File, verbose: bool) -> Self {
+        let mut structure = Structure{..Default::default()};
+        let lines = io::BufReader::new(pdb).lines();
 
-#[allow(dead_code)]
-pub struct AAMolecule {
-    atom_names: Vec<String>,
-    atom_coors: Vec<[f64; 2]>,
-    residues: Vec<AAResidue>,
-    chains: Vec<AAChain>,
-}
+        for line in lines {
+            if let Ok(line) = line {
+                let mut chain_index = 1;
+                if line.starts_with("ATOM") {
+                    let atom_index = line[6..11].trim().to_string();
+                    let atom_index = match atom_index.parse::<u32>() {
+                        Ok(index) => index,
+                        Err(_) => {
+                            if verbose {
+                                println!("├─ {} {} {}", "Error:".red().bold(), "Can not parse atom index -> ", atom_index);
+                                println!("└─ {} '{}'", "Line:".red().bold(), line);
+                            } else {
+                                println!("{} {} {}", "Error:".red().bold(), "Can not parse atom index -> ", atom_index);
+                                println!("{} '{}'", "Line:".red().bold(), line);
+                            }
+                            process::exit(1);
+                        }
+                    };
+                    structure.atom_index.push(atom_index);
 
-#[allow(dead_code)]
-pub struct AACGResidue {
-    res_idx: u32,
-    res_name: String,
-    atm_name: String,
-    atoms: Vec<u64>,
-}
+                    let atom_name = line[12..16].trim().to_string();
+                    structure.atom_name.push(atom_name);
 
-#[allow(dead_code)]
-pub struct AACGChain {
-    first: u32,
-    last: u32,
-    moltype: u32,
-    segname: String,
+                    let residue_name = line[17..20].trim().to_string();
+                    structure.residue_name.push(residue_name);
+
+                    let chain_name = line[21..22].trim().to_string();
+                    structure.chain_name.push(chain_name);
+
+                    let residue_index = line[22..26].trim().to_string();
+                    let residue_index = match residue_index.parse::<u32>() {
+                        Ok(index) => index,
+                        Err(_) => {
+                            if verbose {
+                                println!("├─ {} {} {}", "Error:".red().bold(), "Can not parse residue index -> ", residue_index);
+                                println!("└─ {} '{}'", "Line:".red().bold(), line);
+                            } else {
+                                println!("{} {} {}", "Error:".red().bold(), "Can not parse residue index -> ", residue_index);
+                                println!("{} '{}'", "Line:".red().bold(), line);
+                            }
+                            process::exit(1);
+                        }
+                    };
+                    structure.residue_index.push(residue_index);
+
+                    let r_x = line[30..38].trim().to_string();
+                    let r_x = match r_x.parse::<f32>() {
+                        Ok(x) => x,
+                        Err(_) => {
+                            if verbose {
+                                println!("├─ {} {} {}", "Error:".red().bold(), "Can not parse x position -> ", r_x);
+                                println!("└─ {} '{}'", "Line:".red().bold(), line);
+                            } else {
+                                println!("{} {} {}", "Error:".red().bold(), "Can not parse x position -> ", r_x);
+                                println!("{} '{}'", "Line:".red().bold(), line);
+                            }
+                            process::exit(1);
+                        }
+                    };
+
+                    let r_y = line[38..46].trim().to_string();
+                    let r_y = match r_y.parse::<f32>() {
+                        Ok(y) => y,
+                        Err(_) => {
+                            if verbose {
+                                println!("├─ {} {} {}", "Error:".red().bold(), "Can not parse y position -> ", r_y);
+                                println!("└─ {} '{}'", "Line:".red().bold(), line);
+                            } else {
+                                println!("{} {} {}", "Error:".red().bold(), "Can not parse y position -> ", r_y);
+                                println!("{} '{}'", "Line:".red().bold(), line);
+                            }
+                            process::exit(1);
+                        }
+                    };
+
+                    let r_z = line[46..54].trim().to_string();
+                    let r_z = match r_z.parse::<f32>() {
+                        Ok(z) => z,
+                        Err(_) => {
+                            if verbose {
+                                println!("├─ {} {} {}", "Error:".red().bold(), "Can not parse z position -> ", r_z);
+                                println!("└─ {} '{}'", "Line:".red().bold(), line);
+                            } else {
+                                println!("{} {} {}", "Error:".red().bold(), "Can not parse z position -> ", r_z);
+                                println!("{} '{}'", "Line:".red().bold(), line);
+                            }
+                            process::exit(1);
+                        }
+                    };
+                    structure.position.push([r_x, r_y, r_z]);
+
+                    structure.chain_index.push(chain_index);
+                } else if line.starts_with("TER") {
+                    chain_index += 1;
+                }
+            }
+        }
+
+        structure
+    }
 }
